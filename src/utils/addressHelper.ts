cd528faef7c7fe3c82686ab63a1f9ab70c6ca8bb
Sergey <dev.zaycev@gmail.com>
2022-11-03 13:19:27 +0200

    add AirdropFactory & SaleFactory addresses in Testnet

--- src/utils/addressHelper.ts
+++ src/utils/addressHelper.ts
@@ -1,8 +1,12 @@
 import { ChainId, CURRENT_CHAIN_ID } from './currentChainId'
 
 const addresses: { [key: string]: { [key: number]: string } } = {
-  EnvoysAirdropFactory: {},
-  EnvoysSaleFactory: {},
+  EnvoysAirdropFactory: {
+    [ChainId.Testnet]: '0x68bEb8f54B53A4a4f1FD0AfadFcFe21EC0e6648C',
+  },
+  EnvoysSaleFactory: {
+    [ChainId.Testnet]: '0xCE13afeF478dB995b9B80076268F0beF9A846814',
+  },
 }
 
 type AvaliableAddressKeys = 'EnvoysAirdropFactory' | 'EnvoysSaleFactory'
