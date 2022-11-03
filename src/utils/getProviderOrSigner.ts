3d9673c521ad0394a5d1260a060b84ebf4362c4b
Sergey <dev.zaycev@gmail.com>
2022-11-03 13:19:53 +0200

    implements default Provider in getProviderOrSigner

--- src/utils/getProviderOrSigner.ts
+++ src/utils/getProviderOrSigner.ts
@@ -1,6 +1,12 @@
-import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers'
+import { JsonRpcSigner, Web3Provider, JsonRpcProvider } from '@ethersproject/providers'
 
-export function getProviderOrSigner(library: Web3Provider, account?: string | null): Web3Provider | JsonRpcSigner {
+export function getProviderOrSigner(
+  library?: Web3Provider,
+  account?: string | null,
+): Web3Provider | JsonRpcProvider | JsonRpcSigner {
+  if (!library) {
+    return new JsonRpcProvider(process.env.RPC_URL as string)
+  }
   return typeof account === 'string' ? getSigner(library, account) : library
 }
 
